fd05ac3cee98cf3114afacf3d49984ca24d55dc4
LukaSerafimovski <lukakumanovac@gmail.com>
2025-02-18 17:18:47 +0100

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,17 @@
+Const kilometre = prompt ("Enter kilometre")
+const factor = 0.621731
+connst milie = kilometre*factor
+
+console.log(miles)
+
+let f = prompt("enter the value of fahrenheit")
+let c = (f - 32) / 1.8;
+document.write("Celsius= ",c);
+
+Const lbs = prompt ("Enter lbs ")
+const factor = 0.621731
+connst kg = Kilogram*factor
+
+Const ft = prompt ("Enter ft ")
+const factor = 0.621731
+connst m = metre*factor
